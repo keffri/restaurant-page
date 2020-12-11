394f15010c90232dc99d869ca9142451b9d2192d
keffri <keffri.w.neal@gmail.com>
2020-12-10 18:42:58 -0800

    create main page test

--- src/modules/mainpage.js
+++ src/modules/mainpage.js
@@ -9,7 +9,7 @@ function createMain() {
   header.classList.add("header");
   let title = document.createElement("h1");
   title.classList.add("title");
-  title.textContent = "Restuarant Page";
+  title.textContent = `Restuarant Page`;
 
   let main = document.createElement("main");
   main.classList.add("body");
@@ -17,8 +17,16 @@ function createMain() {
   foodImg.src = "./images/IMG_6594small.jpg";
 
   let foodText = document.createElement("p");
+  foodText.textContent = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam
+  molestias quasi doloremque ad, amet ipsa sed harum quis delectus
+  cumque odio tempora non vero porro ullam quos perferendis! Maiores
+  quae voluptas, eos voluptate dignissimos nisi molestias illo
+  reprehenderit consequuntur fugiat eligendi, necessitatibus enim
+  molestiae laudantium distinctio corrupti accusantium eum.
+  Architecto?`;
   foodText.classList.add("foodText");
 
+  document.body.appendChild(mainContainer);
   mainContainer.appendChild(content);
   content.appendChild(header);
   header.appendChild(title);
