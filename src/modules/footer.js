c0da230a3fdb12dcba74996ae0a53dc6a39370c6
keffri <keffri.w.neal@gmail.com>
2020-12-13 14:24:19 -0800

    fixed positioning

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -4,11 +4,15 @@ function renderFooter() {
   let footer = document.createElement("footer");
   footer.classList.add("footer");
 
+  let footerLink = document.createElement("a");
+  footerLink.href = "https://www.github.com/keffri";
+  footerLink.setAttribute("target", "_blank");
   let gitHubIcon = document.createElement("i");
   gitHubIcon.classList.add("fab");
-  gitHubIcon.classList.add("fa-github-square");
+  gitHubIcon.classList.add("fa-github");
+  footerLink.appendChild(gitHubIcon);
 
-  footer.appendChild(gitHubIcon);
+  footer.appendChild(footerLink);
 
   mainContainer.appendChild(footer);
 }
