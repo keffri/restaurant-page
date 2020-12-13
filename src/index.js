dc4b6549c300e8ee4891d778b3f16c078da5ea62
keffri <keffri.w.neal@gmail.com>
2020-12-13 14:24:04 -0800

    added home

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { renderNav } from "./modules/navbar";
 import { renderFooter } from "./modules/footer";
+import { renderHome } from "./modules/home";
 
 const mainContainer = document.getElementById("mainContainer");
 
 function home() {
   renderNav();
+  renderHome();
   renderFooter();
 }
 
