98694d40e6b7a1744dfae636a3ee1dd34b8fee24
keffri <keffri.w.neal@gmail.com>
2020-12-15 18:23:24 -0800

    changed title

--- src/modules/home.js
+++ src/modules/home.js
@@ -9,15 +9,21 @@ function renderHome() {
 
   let homeTitle = document.createElement("h3");
   homeTitle.classList.add("homeTitle");
-  homeTitle.textContent = "Restaurant Page";
+  homeTitle.textContent = "Bob's Burgers";
 
-  let homeText = document.createElement("p");
-  homeText.classList.add("homeText");
-  homeText.textContent =
-    " Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+  // let homeText = document.createElement("p");
+  // homeText.classList.add("homeText");
+  // homeText.textContent =
+  //   " Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+
+  let homeImg = document.createElement("img");
+  homeImg.classList.add("homeImg");
+  homeImg.src =
+    "../dist/images/BobsBurgers_1005_BobBelcherAndTheTerribleHorrible..._A05_Promo_08.jpg";
 
   home.appendChild(homeTitle);
-  home.appendChild(homeText);
+  // home.appendChild(homeText);
+  home.appendChild(homeImg);
   homeSection.appendChild(home);
 
   mainContainer.appendChild(homeSection);
