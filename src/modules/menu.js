f4f81161d536a45725f5c84031996bff5baccf44
keffri <keffri.w.neal@gmail.com>
2020-12-17 18:34:37 -0800

    updated menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -16,53 +16,67 @@ function renderMenu() {
 
   /*** MENU ITEMS ***/
 
-  let menuItemOne = document.createElement("div");
-  menuItemOne.classList.add("menuItem");
+  let menuItems = document.createElement("div");
+  menuItems.classList.add("menuDiv");
+  let regular = document.createElement("p");
+  regular.textContent = "REGULAR";
+  menuItems.appendChild(regular);
+  let special = document.createElement("p");
+  special.textContent = "SPECIAL";
+  menuItems.appendChild(special);
+  let cheese = document.createElement("p");
+  cheese.textContent = "CHEESE";
+  menuItems.append(cheese);
+  let fries = document.createElement("p");
+  fries.textContent = "FRIES";
+  menuItems.appendChild(fries);
+  let sweetPotatoFries = document.createElement("p");
+  sweetPotatoFries.classList.add("s");
+  sweetPotatoFries.textContent = "Sweet Potato Fries";
+  menuItems.appendChild(sweetPotatoFries);
+  let sideSalad = document.createElement("p");
+  sideSalad.textContent = "SIDE SALAD";
+  menuItems.appendChild(sideSalad);
+  let softDrink = document.createElement("p");
+  softDrink.textContent = "SOFT DRINK";
+  menuItems.appendChild(softDrink);
+  let beer = document.createElement("p");
+  beer.textContent = "BEER";
+  menuItems.appendChild(beer);
 
-  let menuItemTitle = document.createElement("p");
-  menuItemTitle.classList.add("menuItemTitle");
-  menuItemTitle.textContent = "BURGER";
-  let menuItemTitleSub = document.createElement("p");
-  menuItemTitleSub.classList.add("menuItemTitleSub");
-  menuItemTitleSub.textContent = "OF THE DAY";
+  /*** MENU PRICES ***/
 
-  let menuItemTwo = document.createElement("div");
-  menuItemTwo.classList.add("menuItem");
-  let menuItemTwoText = document.createElement("p");
-  menuItemTwoText.classList.add("menuItemText");
-  menuItemTwoText.textContent = `"Byte Me" Burger`;
+  let menuPrices = document.createElement("div");
+  menuPrices.classList.add("menuDiv");
+  let regularPrice = document.createElement("p");
+  regularPrice.textContent = "$5.00";
+  menuPrices.appendChild(regularPrice);
+  let specialPrice = document.createElement("p");
+  specialPrice.textContent = "$5.95";
+  menuPrices.appendChild(specialPrice);
+  let cheesePrice = document.createElement("p");
+  cheesePrice.textContent = "ADD.50";
+  menuPrices.appendChild(cheesePrice);
+  let friesPrice = document.createElement("p");
+  friesPrice.textContent = "$2.00";
+  menuPrices.appendChild(friesPrice);
+  let sweetPotatoFriesPrice = document.createElement("p");
+  sweetPotatoFriesPrice.textContent = "$3.00";
+  menuPrices.appendChild(sweetPotatoFriesPrice);
+  let sideSaladPrice = document.createElement("p");
+  sideSaladPrice.textContent = "$2.50";
+  menuPrices.appendChild(sideSaladPrice);
+  let softDrinkPrice = document.createElement("p");
+  softDrinkPrice.textContent = "$2.00";
+  menuPrices.appendChild(softDrinkPrice);
+  let beerPrice = document.createElement("p");
+  beerPrice.textContent = "$4.00";
+  menuPrices.appendChild(beerPrice);
 
-  let menuItemThree = document.createElement("div");
-  menuItemThree.classList.add("menuItem", "fries");
-  let menuItemThreeTextOne = document.createElement("p");
-  menuItemThreeTextOne.classList.add("menuItemText");
-  menuItemThreeTextOne.textContent = "Fries";
-  let menuItemThreeTextTwo = document.createElement("p");
-  menuItemThreeTextTwo.classList.add("menuItemText", "s");
-  menuItemThreeTextTwo.textContent = "Sweet Potato Fries";
-
-  let menuItemFour = document.createElement("div");
-  menuItemFour.classList.add("menuItem");
-  let menuItemFourText = document.createElement("p");
-  menuItemFourText.classList.add("menuItemText");
-  menuItemFourText.textContent = "$5.95";
+  menuContainer.appendChild(menuItems);
+  menuContainer.appendChild(menuPrices);
 
   menu.appendChild(menuTitle);
-
-  menuItemOne.appendChild(menuItemTitle);
-  menuItemOne.appendChild(menuItemTitleSub);
-
-  menuItemTwo.appendChild(menuItemTwoText);
-
-  menuItemThree.appendChild(menuItemThreeTextOne);
-  menuItemThree.appendChild(menuItemThreeTextTwo);
-
-  menuItemFour.appendChild(menuItemFourText);
-
-  menuContainer.appendChild(menuItemOne);
-  menuContainer.appendChild(menuItemTwo);
-  menuContainer.appendChild(menuItemThree);
-  menuContainer.appendChild(menuItemFour);
 
   menu.appendChild(menuContainer);
 
